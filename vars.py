9814ca8d8b9a791d30246bf1e970faadf8e679f4
spsmain <129521948+spsmain@users.noreply.github.com>
2025-05-20 05:16:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23673651"))
-API_HASH = environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
+API_ID = int(environ.get("API_ID", "25033617"))
+API_HASH = environ.get("API_HASH", "4c6d9c4757c0c253c680e284b17d7f54")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
